789450847807d46bbd4a74ef5e9d2b4bef134e43
Michael Tatum <commonslaughter@users.noreply.github.com>
2021-12-27 15:33:55 -0600

    Update firstRun-DEBIAN-SERVER.bash

--- firstRun-DEBIAN-SERVER.bash
+++ firstRun-DEBIAN-SERVER.bash
@@ -1,1 +1,42 @@
 #!/bin/bash
+## 
+## Setup a local personal device after a clean Debian install
+
+## viking mode for safety
+set -e
+
+date
+
+## update the system to current packages
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && sudo apt upgrade -y
+
+## setup yubikeys; https://support.yubico.com/support/solutions/articles/15000010964-enabling-the-yubico-ppa-on-ubuntu
+## sudo add-apt-repository ppa:yubico/stable && sudo apt-get update
+## sudo apt-get install -y yubikey-manager-qt
+
+## generate a fresh set of SSH keys
+## ssh-keygen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+date
+echo
+echo "It took $SECONDS seconds for this script to execute."
+
+## final reboot as last action; enables newest kernel
+## sudo reboot now
+
+## EoF
