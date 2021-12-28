13fba6d429ad8e9ed76f238f54546489eb48a880
Michael Tatum <commonslaughter@users.noreply.github.com>
2021-12-28 14:19:02 -0600

    Update firstRun-DEBIAN-SERVER.bash

--- firstRun-DEBIAN-SERVER.bash
+++ firstRun-DEBIAN-SERVER.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-## 
-## Setup a local personal server after a clean Debian install
+## [ insert your name ]
+## Setup a local personal server after a clean RHEL install
 
 ## viking mode for safety
 set -e
@@ -11,37 +11,58 @@ timedatectl set-timezone America/Chicago
 date
 
 ## update the system to current packages
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt update && sudo apt upgrade -y
+sudo yum update -y
+sudo yum upgrade -y
 
 ## generate a fresh set of SSH keys
 ## ssh-keygen
 
-## install management tools
-## sudo apt-get install -y snmpd            ## for observium
-## sudo apt-get install -y salt-minion      ## for saltstack
-## sudo apt-get install -y salt-master      ## for saltstack
-## sudo apt-get install -y chef
-## sudo apt-get install -y ansible
-## sudo apt-get install -y cockpit
+## install system tools
+## sudo yum install -y snmpd            
+sudo yum install -y salt-minion      
+## sudo yum install -y salt-master      
+## sudo yum install -y chef
+## sudo yum install -y ansible
+sudo yum install -y cockpit
+sudo yum install -y net-tools
+sudo yum install -y nmap
+sudo yum install -y iptables
+sudo yum install -y iptables-services
+sudo yum install -y htop
+sudo yum install -y git
+sudo yum install -y backupninja
+sudo yum install -y lynis
+sudo yum install -y rkhunter
 
+## install LAMP packages for services 
+## sudo yum install httpd mod_evasive mod_ssl ghostscript postfix
+## sudo yum install mariadb mariadb-server nano tar curl
+## sudo yum install php php-mysql php-iconv php-mbstring php-curl php-openssl php-tokenizer
+## sudo yum install php-simplexml php-spl php-pcre php-dom php-xml php-intl php-json 
+## sudo yum install  php-xmlpc php-soap php-ctype php-zip php-gd php-ldap php-pecl-apc
 
+## Start and enable services
+## systemctl enable httpd
+## systemctl start httpd
+## systemctl enable mariadb
+## systemctl start mariadb
+## service cockpit start
+## systemctl enable cockpit 
+## service postfix start
+## systemctl enable postfix
 
-
-
-
-
-
-
-
-
-
+## show status of new services 
+## service iptables status
+## service httpd status 
+## service cockpit status
+## service postfix status
+## service mariadb status
 
 date
 echo
 echo "It took $SECONDS seconds for this script to execute."
 
-## final reboot as last action; enables newest kernel
+## final reboot as last action; enables newer kernel
 ## sudo reboot now
 
 ## EoF
