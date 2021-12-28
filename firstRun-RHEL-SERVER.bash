6fc4f5453993e85cb402493f7024ee91f59e8ee1
Michael Tatum <commonslaughter@users.noreply.github.com>
2021-12-28 13:48:15 -0600

    Update firstRun-RHEL-SERVER.bash

--- firstRun-RHEL-SERVER.bash
+++ firstRun-RHEL-SERVER.bash
@@ -16,11 +16,11 @@ sudo yum upgrade -y
 
 ## install system tools
 ## sudo yum install -y snmpd            
-## sudo yum install -y salt-minion      
+sudo yum install -y salt-minion      
 ## sudo yum install -y salt-master      
 ## sudo yum install -y chef
 ## sudo yum install -y ansible
-## sudo yum install -y cockpit
+sudo yum install -y cockpit
 sudo yum install -y net-tools
 ## sudo yum install -y nmap
 sudo yum install -y iptables
@@ -31,15 +31,51 @@ sudo yum install -y git
 sudo yum install -y lynis
 sudo yum install -y rkhunter
 
+## configure IPTables firewall
+yum install iptables -y
+yum install iptables-services -y
+systemctl enable iptables
 
+## deactivate firewalld
+service firewalld stop
+systemctl disable firewalld 
+service iptables start
 
+## flush existing rules
+iptables -F; iptables -X; iptables -Z
 
+## accept established inbound traffic 
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+## configure loopback traffic
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -d 127.0.0.0/8 -j REJECT
 
+## Block some common attacks
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 
+## allow SSH
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
+## allow ping
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
+## allow HTTP(s)
+## iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+## iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 
+## set default policy 
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD DROP
+
+## save the changes
+service iptables save
+
+## show rules
+iptables -L -n
 
 date
 echo
