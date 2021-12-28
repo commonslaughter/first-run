dd8332326a75ffeb521176d8dd73dab3ae0e0a17
Michael Tatum <commonslaughter@users.noreply.github.com>
2021-12-28 12:22:10 -0600

    Update firstRun-DEBIAN-SERVER.bash

--- firstRun-DEBIAN-SERVER.bash
+++ firstRun-DEBIAN-SERVER.bash
@@ -11,10 +11,6 @@ date
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt update && sudo apt upgrade -y
 
-## setup yubikeys; https://support.yubico.com/support/solutions/articles/15000010964-enabling-the-yubico-ppa-on-ubuntu
-## sudo add-apt-repository ppa:yubico/stable && sudo apt-get update
-## sudo apt-get install -y yubikey-manager-qt
-
 ## generate a fresh set of SSH keys
 ## ssh-keygen
 
