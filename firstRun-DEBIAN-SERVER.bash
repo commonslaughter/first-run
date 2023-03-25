80579d7750f8bbb8fb95ae5039be5f8d17a8c6ab
Michael Tatum <commonslaughter@users.noreply.github.com>
2023-03-25 14:51:53 -0500

    Update firstRun-DEBIAN-SERVER.bash

--- firstRun-DEBIAN-SERVER.bash
+++ firstRun-DEBIAN-SERVER.bash
@@ -3,7 +3,7 @@
 ## Setup a local personal server after a clean RHEL install
 
 ## viking mode for safety
-set -e
+## set -e
 
 ## set the timezone
 timedatectl set-timezone America/Chicago
@@ -22,7 +22,7 @@ sudo apt install -y snmpd
 sudo apt install -y salt-minion      
 ## sudo apt install -y salt-master      
 ## sudo apt install -y chef
-## sudo apt install -y ansible
+sudo apt install -y ansible
 sudo apt install -y cockpit
 sudo apt install -y net-tools
 sudo apt install -y nmap
@@ -39,28 +39,28 @@ sudo apt install -y nano
 sudo apt install -y wget
 
 ## install LAMP packages for services 
-## sudo apt install httpd mod_evasive mod_ssl ghostscript postfix
-## sudo apt install mariadb mariadb-server   
-## sudo apt install php php-mysql php-iconv php-mbstring php-curl php-openssl php-tokenizer
-## sudo apt install php-simplexml php-spl php-pcre php-dom php-xml php-intl php-json 
-## sudo apt install  php-xmlpc php-soap php-ctype php-zip php-gd php-ldap php-pecl-apc
+sudo apt install httpd mod_evasive mod_ssl ghostscript postfix
+sudo apt install mariadb mariadb-server   
+sudo apt install php php-mysql php-iconv php-mbstring php-curl php-openssl php-tokenizer
+sudo apt install php-simplexml php-spl php-pcre php-dom php-xml php-intl php-json 
+sudo apt install  php-xmlpc php-soap php-ctype php-zip php-gd php-ldap php-pecl-apc
 
 ## Start and enable services
-## systemctl enable httpd
-## systemctl start httpd
-## systemctl enable mariadb
-## systemctl start mariadb
+systemctl enable httpd
+systemctl start httpd
+systemctl enable mariadb
+systemctl start mariadb
 service cockpit start
 systemctl enable cockpit 
-## service postfix start
-## systemctl enable postfix
+service postfix start
+systemctl enable postfix
 
 ## show status of new services 
-## service iptables status
-## service httpd status 
+service iptables status
+service httpd status 
 service cockpit status
-## service postfix status
-## service mariadb status
+service postfix status
+service mariadb status
 
 date
 echo
