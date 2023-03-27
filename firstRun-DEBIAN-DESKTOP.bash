e19ef2bbe87f71c8d900204f8700d434369e6848
Michael Tatum <commonslaughter@users.noreply.github.com>
2023-03-27 18:48:16 -0500

    Update firstRun-DEBIAN-DESKTOP.bash

--- firstRun-DEBIAN-DESKTOP.bash
+++ firstRun-DEBIAN-DESKTOP.bash
@@ -1,104 +1,91 @@
 #!/bin/bash
 ## [ insert your name ]
-## Setup a local personal device after a clean Debian install
+## Setup a local personal server after a clean RHEL install
 
 ## viking mode for safety
-## set -e
+## sudo set -e
 
 ## set the timezone
-timedatectl set-timezone America/Chicago
+sudo timedatectl set-timezone America/Chicago
 
 date
 
-## update the system to current packages
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt update && sudo apt upgrade -y
+## set your hostname
+## sudo hostnamectl set-hostname [ insert your name ]
 
-## setup yubikeys; https://support.yubico.com/support/solutions/articles/15000010964-enabling-the-yubico-ppa-on-ubuntu
-## sudo add-apt-repository ppa:yubico/stable && sudo apt-get update
-## sudo apt-get install -y yubikey-manager-qt
+## save your local subnet
+## sudo nano /etc/hosts
+## sudo nano /etc/motd
+
+## update the system to current packages
+sudo apt update -y
+sudo apt upgrade -y
 
 ## generate a fresh set of SSH keys
-## ssh-keygen
+ssh-keygen
 
-## install network tools
-sudo apt-get install -y firefox
-sudo apt-get install -y chromium-browser
-sudo apt-get install -y filezilla
-sudo apt-get install -y deluge
-sudo apt-get install -y transmission
-sudo apt-get install -y midori
-## sudo apt-get install -y zenmap
-sudo apt-get install -y net-tools
-sudo apt-get install -y nmap
-sudo apt-get install -y iptables
-## sudo apt-get install -y iptables-services
-sudo apt-get install -y remmina
+## generate keys for github
+## ssh-keygen -t ed25519 -C "[ insert your name ]"
 
 ## install system tools
-sudo apt-get install -y htop
-sudo apt-get install -y gnome-disk-utility
-sudo apt-get install -y usb-creator-gtk
-sudo apt-get install -y tilix
-sudo apt-get install -y xfburn
-sudo apt-get install -y gparted
-sudo apt-get install -y git
+sudo apt install -y snmpd            
+sudo apt install -y salt-minion      
+## sudo apt install -y salt-master      
+## sudo apt install -y chef
+sudo apt install -y ansible
+sudo apt install -y cockpit
+sudo apt install -y net-tools
+sudo apt install -y nmap
+sudo apt install -y iptables
+sudo apt install -y iptables-services
+sudo apt install -y htop
+sudo apt install -y git
+sudo apt install -y backupninja
+sudo apt install -y lynis
+sudo apt install -y rkhunter
+sudo apt install -y curl
+sudo apt install -y tar
+sudo apt install -y nano
+sudo apt install -y wget
 
-## install office tools
-sudo apt-get install -y xarchiver
-## sudo apt-get install -y 7zip
-sudo apt-get install -y featherpad
-sudo apt-get install -y gedit
-sudo apt-get install -y libreoffice
-sudo apt-get install -y geany
+## install LAMP packages for services 
+sudo apt install httpd mod_evasive mod_ssl ghostscript postfix
+sudo apt install mariadb mariadb-server   
+sudo apt install php php-mysql php-iconv php-mbstring php-curl php-openssl php-tokenizer
+sudo apt install php-simplexml php-spl php-pcre php-dom php-xml php-intl php-json 
+sudo apt install  php-xmlpc php-soap php-ctype php-zip php-gd php-ldap php-pecl-apc
 
-## install multimedia tools
-sudo apt-get install -y redshift-gtk
-## sudo apt-get install -y geoclue-2.0
-sudo apt-get install -y vlc
-sudo apt-get install -y rhythmbox
-## sudo apt-get install -y shutter
-sudo apt-get install -y gimp
-sudo apt-get install -y cheese
-sudo apt-get install -y openshot
-sudo apt-get install -y obs-studio
+## Start and enable services
+sudo systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
+sudo service cockpit start
+sudo systemctl enable cockpit 
+sudo service postfix start
+sudo systemctl enable postfix
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo systemctl start iptables
 
-## install container tools
-## sudo apt-get install -y virtualbox
-## sudo apt-get install -y snapd
-## sudo apt-get install -y lxc
+## show status of new services 
+sudo service iptables status
+sudo iptables -L
+sudo service httpd status 
+sudo service cockpit status
+sudo service postfix status
+sudo service mariadb status
 
-## install game tools
-sudo apt-get install -y pychess
-sudo apt-get install -y steam
-sudo apt-get install -y lbreakout2
-
-## install backup tools
-sudo apt-get install -y luckybackup
-## sudo apt-get install -y backupninja
-
-## install security tools
-sudo apt-get install -y keypassx
-sudo apt-get install -y keypassxc
-sudo apt-get install -y clamav clamav-daemon clamtk
-sudo apt-get install -y bleachbit
-sudo apt-get install -y lynis
-sudo apt-get install -y rkhunter
-sudo apt-get install -y gtkhash
-
-## install management tools
-## sudo apt-get install -y snmpd            ## for observium
-## sudo apt-get install -y salt-minion      ## for saltstack
-## sudo apt-get install -y salt-master      ## for saltstack
-## sudo apt-get install -y chef
-## sudo apt-get install -y ansible
-## sudo apt-get install -y cockpit
+## First-Time Git Setup
+## sudo git config --global user.name "[ insert your name ]"
+## sudo git config --global user.email [ insert your name ]
+## sudo git config --global core.editor nano
 
 date
 echo
 echo "It took $SECONDS seconds for this script to execute."
 
-## final reboot as last action; enables newest kernel
+## final reboot as last action; enables newer kernel
 ## sudo reboot now
 
 ## EoF
