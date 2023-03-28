11943af9faa2fa04edc2aadceaa69a625d8e60fc
Michael Tatum <commonslaughter@users.noreply.github.com>
2023-03-27 19:16:35 -0500

    Update firstRun-DEBIAN-DESKTOP.bash

--- firstRun-DEBIAN-DESKTOP.bash
+++ firstRun-DEBIAN-DESKTOP.bash
@@ -21,16 +21,21 @@ date
 sudo apt update -y
 sudo apt upgrade -y
 
-## generate a fresh set of SSH keys
-ssh-keygen
-
-## generate keys for github
-## ssh-keygen -t ed25519 -C "[ insert your name ]"
-
-## install system tools
-sudo apt install -y snmpd            
-sudo apt install -y salt-minion      
-## sudo apt install -y salt-master      
+## install network tools
+sudo apt install -y firefox
+sudo apt install -y chromium-browser
+sudo apt install -y filezilla
+sudo apt install -y deluge
+sudo apt install -y transmission
+sudo apt install -y midori
+## sudo apt install -y zenmap
+sudo apt install -y net-tools
+sudo apt install -y nmap
+sudo apt install -y iptables
+sudo apt install -y iptables-services
+sudo apt install -y remmina
+sudo apt install -y salt-minion
+## sudo apt install -y salt-master
 ## sudo apt install -y chef
 sudo apt install -y ansible
 sudo apt install -y cockpit
@@ -38,15 +43,74 @@ sudo apt install -y net-tools
 sudo apt install -y nmap
 sudo apt install -y iptables
 sudo apt install -y iptables-services
+sudo apt install -y wget
+sudo apt install -y snmpd
+sudo apt install -y curl
+
+## install system tools
 sudo apt install -y htop
+sudo apt install -y gnome-disk-utility
+sudo apt install -y usb-creator-gtk
+sudo apt install -y tilix
+sudo apt install -y xfburn
+sudo apt install -y gparted
 sudo apt install -y git
+sudo apt install -y htop
 sudo apt install -y backupninja
 sudo apt install -y lynis
 sudo apt install -y rkhunter
-sudo apt install -y curl
 sudo apt install -y tar
 sudo apt install -y nano
-sudo apt install -y wget
+
+## install office tools
+sudo apt install -y xarchiver
+sudo apt install -y 7zip
+sudo apt install -y featherpad
+sudo apt install -y gedit
+sudo apt install -y libreoffice
+sudo apt install -y geany
+
+## install multimedia tools
+sudo apt install -y redshift-gtk
+## sudo apt install -y geoclue-2.0
+sudo apt install -y vlc
+sudo apt install -y rhythmbox
+## sudo apt install -y shutter
+sudo apt install -y gimp
+sudo apt install -y cheese
+sudo apt install -y openshot-qt
+sudo apt install -y obs-studio
+
+## install container tools
+## sudo apt install -y virtualbox
+sudo apt install -y snapd
+## sudo apt install -y lxc
+## sudo apt install -y flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+## install game tools
+sudo apt install -y pychess
+sudo apt install -y steam
+sudo apt install -y lbreakout2
+
+## install backup tools
+sudo apt install -y luckybackup
+sudo apt install -y backupninja
+
+## install security tools
+sudo apt install -y keypassx
+sudo apt install -y keypassxc
+sudo apt install -y clamav clamav-daemon clamtk
+sudo apt install -y bleachbit
+sudo apt install -y lynis
+sudo apt install -y rkhunter
+sudo apt install -y gtkhash
+
+## generate a fresh set of SSH keys
+ssh-keygen
+
+## generate keys for github
+## ssh-keygen -t ed25519 -C "[ insert your name ]"
 
 ## First-Time Git Setup
 ## sudo git config --global user.name "[ insert your name ]"
